0b562a3a426288da95d9cba751bbd308e568102b
Maximilian Tabet <maxtabet@gmail.com>
2020-12-21 23:44:47 -0500

    commit tree 33703338a246436e53c00ea1853b576150b6860b

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+        @artist = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.new_by_filename(file_name)
+        name = file_name.split(" - ")[1]
+        artist_name = file_name.split(" - ").first
+        song = self.new(name)
+        song.artist_name = artist_name
+        song
+    end
+
+    def artist_name=(artist_name)
+        self.artist = Artist.find_or_create_by_name(artist_name)
+        self.artist.add_song(self)
+    end
+    
+    # def self.new_by_filename(file_name)
+    #     song = Song.new(filename.split(" - ")[1])
+    #     song.artist_name(filename.split(" - ")[0])
+    #     song
+    # end
+
+end
